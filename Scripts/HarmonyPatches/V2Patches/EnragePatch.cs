672bfeaf2695f106bcf7e30b1ea0492a362f4e4e
Kyryh <71778401+Kyryh@users.noreply.github.com>
2025-03-29 21:58:28 +0100

    Fixed V2's enrage not working correctly

--- Scripts/HarmonyPatches/V2Patches/EnragePatch.cs
+++ Scripts/HarmonyPatches/V2Patches/EnragePatch.cs
@@ -3,16 +3,17 @@ using HarmonyLib;
 
 namespace WesV2.Patches;
 
-[HarmonyPatch(typeof(V2), "Enrage")]
+[HarmonyPatch(typeof(V2))]
+[HarmonyPatch(nameof(V2.Enrage), [typeof(string)])]
 class Patch5 : DefaultPatch {
-    static void Postfix(V2 __instance) {
-        if (__instance.secondEncounter) {
+    static void Postfix(V2 __instance, string enrageName) {
+        if (enrageName == "STOP HITTING YOURSELF") {
             ExtensionMethods.V2AdditionalData data = __instance.GetAdditionalData();
             data.timesChangedWeapon = 0;
             __instance.PlayVoice(data.enrageAudioClip);
             MonoSingleton<SubtitleController>.Instance.DisplaySubtitle(data.enrageSubtitle);
         }
 
-        Plugin.LogDebug("v2 enraged");
+        Plugin.LogDebug($"v2 enraged (enrageName=\"{enrageName}\")");
     }
 }
